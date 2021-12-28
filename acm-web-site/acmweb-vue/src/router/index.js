b3da50ef861194b00e6de3eaa609c8fa4ab787a1
OREO-code <2319841923@qq.com>
2021-12-28 19:21:31 +0800

    tijiao

---
+++ acm-web-site/acmweb-vue/src/router/index.js
@@ -0,0 +1,63 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Form from '../views/Form.vue'
+import Tulin from '../views/Tulin.vue'
+import Member from '../views/member.vue'
+import Time from '../views/Time.vue'
+import Login from '../views/Login.vue'
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/form',
+    name: 'Form',
+    component: Form
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/tulin',
+    name: 'Tulin',
+    component: Tulin
+  },
+  {
+    path: '/member',
+    name: 'Member',
+    component: Member
+  },
+  {
+    path: '/time',
+    name: 'Time',
+    component: Time
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
---
+++ acm-web-site/acmweb-vue/src/views/Time.vue
@@ -0,0 +1,82 @@
+<template>
+	<div id="app">
+		  	<div class="block">
+		  	  <el-timeline>
+		  	    <el-timeline-item v-for="item in timeList" :timestamp="item.year" placement="top">
+		  	      <el-card>
+		  	        <p>{{item.content}}</p>
+		  	      </el-card>
+		  	    </el-timeline-item>
+		  	  </el-timeline>
+		  	</div>
+	</div>
+</template>
+<!-- </el-row> -->
+<script>
+	export default {
+	    data() {
+	      return {
+	        reverse: true,
+			sum:0,
+	        timeList:[]
+	      };
+	    },
+		created() {
+			var _that=this
+			this.$axios.post('http://101.43.16.42:8082/time').then((response)=>{
+					_that.notice=response.data.data.notice
+			        console.log(response);
+			      }).catch((response)=>{
+			        console.log(response);
+			      })
+		}
+	  };	
+</script>
+
+<style>
+	#app {
+	  font-family: Avenir, Helvetica, Arial, sans-serif;
+	  -webkit-font-smoothing: antialiased;
+	  -moz-osx-font-smoothing: grayscale;
+	  text-align: center;
+	  color: #2c3e50;
+	}
+	.top{
+		width: 100%;
+		height: auto;
+	}
+	#nav {
+		width: 100%;
+	}
+	
+	#nav a {
+	  font-weight: bold;
+	  color: #2c3e50;
+	}
+	
+	#nav a.router-link-exact-active {
+	  color: #42b983;
+	}
+	//时间线
+	.item {
+	  margin-left: 30px; /*用左边margin显示时间线*/
+	  width: calc(100% - 30px); /*因为左边部分用于显示时间线，所以右边部分减去30px*/
+	  height: auto; /*高度由内容决定*/
+	  position: relative;
+	  margin-bottom: 10px;
+	  cursor: pointer;
+	}
+	.item::before {
+	  content: "";
+	  width: 11px;
+	  height: 11px;
+	  border-radius: 100%;
+	  background-color: #91c2fc;
+	  position: absolute;
+	  left: -15px;
+	}
+	.block{
+		margin-top: 5%;
+	}
+
+</style>
