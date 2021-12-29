d61c89142513d702bc1c503fc2edfc143f383207
zeroacmer <10791894@qq.com>
2021-12-29 18:04:21 +0800

    添加前端

---
+++ acmweb-vue/src/App.vue
@@ -0,0 +1,187 @@
+<template>
+  <div id="app">
+	  	<div id="nav">
+	  		<el-menu
+						router="true"
+	  		          :default-active="activeIndex2"
+	  		          class="el-menu-demo"
+	  		          mode="horizontal"
+	  		          @select="handleSelect"
+	  		          >
+	  		        <el-menu-item index="1" route="/"><div >首页</div></el-menu-item>
+	  				<!-- <el-menu-item index="2" route="/time"><div >时间线</div></el-menu-item> -->
+	  				<!-- <el-menu-item index="3" route="/form">组织架构</el-menu-item> -->
+	  				<el-menu-item index="2" route="/member"><div>成员风采</div></el-menu-item>
+	  		        <!-- <el-submenu index="2">
+	  		          <template slot="title">组织架构</template>
+	  		          <el-menu-item index="2-1">竞赛队</el-menu-item>
+	  		          <el-menu-item index="2-2">技术部</el-menu-item>
+	  		          <el-menu-item index="2-3">预备队</el-menu-item>
+	  		        </el-submenu> -->
+	  		        <el-menu-item index="3" route="/tulin"><div>图灵杯</div></el-menu-item>
+	  		        <el-menu-item index="4"><a href="http://newoj.acmclub.cn/home" style="text-decoration: none;color: #A6ADB4;" target="_blank" ><div>NEUQ OJ</div></a>
+	  		        </el-menu-item>
+					<!-- <el-menu-item index="7" route="/login"><div>管理员登录</div></el-menu-item> -->
+	  		        <!--        <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>-->
+	  		      </el-menu>
+	  	</div>
+		<router-view></router-view>
+	  
+  </div>
+</template>
+
+<style>
+#app {
+	margin-top: -1%;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+/* 菜单栏 */
+/* .el-menu-item :hover{
+	color: white;
+} */
+.top{
+	width: 100%;
+	height: auto;
+}
+.topitem :hover{
+	background-color: #1890FF;
+}
+#nav {
+	width: 100%;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+.el-carousel__container{
+	width: 60%;
+	margin-left: 20%;
+	margin-top: 3.125rem;
+}
+.el-carousel__item{
+	width: 100%;
+}
+.player{
+	width: 100%;
+	height: 3.125rem;
+	background-color: #FFEBCD;
+	filter: Alpha(Opacity=50); /* 针对IE*/
+	background-color: rgba(0, 0, 0, 0.5); /*针对其他浏览器*/
+	top: 0%;
+	line-height: 3.125rem;
+}
+.el-carousel__item h1 {
+    color: #475669;
+    font-size: 18px;
+    opacity: 0.75;
+    line-height: 300px;
+    margin: 0;
+  }
+  .el-carousel__item:nth-child(2n) {
+    background-color: #D4DFE7;
+  }
+  .el-carousel__item:nth-child(2n+1) {
+    background-color: #FFFAE8;
+  }
+  .bannerImg{
+      width: 100%;
+      height: inherit;
+      min-height: 360px;
+      min-width: 1400px;
+    }
+	body{
+		width: 100%;
+		background-color: #fff;
+	}
+	.showintro{
+		box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+		width: 80%;
+		margin-left: 10%;
+		height: 500px;
+		background-color: #FFFFFF;
+		margin-top: 3%;
+	}
+	.notice{
+		width: 100%;
+		height: 3.125rem;
+		line-height: 3.125rem;
+		text-align: left;
+	}
+	@-webkit-keyframes horse {
+		0% {
+			left: 0%;
+			top: 0px;
+			background-color: lavenderblush;
+		}
+	 
+		47% {
+			left: 105%;
+			top: 0px;
+		}
+	 
+		48% {
+			left: 105%;
+			top: 100px;
+		}
+	 
+		49% {
+			left: -105%;
+			top: 100px;
+		}
+	 
+		50% {
+			left: -100%;
+			top: 0px;
+			background-color: lightpink;
+		}
+	 
+		100% {
+			left: 0%;
+			top: 0px;
+			background-color: #FFB6C1;
+		}
+	}
+		
+	.showImage{
+		width: 100%;
+		height: 100%;
+	}
+</style>
+<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+<script>
+	export default {
+	    data() {
+	      return {
+	        activeIndex: '1',
+	        activeIndex2: '1',
+			showImageList:[],
+			notice:'',
+	      };
+	    },
+		methods:{
+			getInfo(){
+				console.log('执行')
+				axios.get('http://101.43.16.42:8082/notice')
+				  .then(function (response) {
+				    console.log(response);
+					console.log('已执行')
+				  })
+				  .catch(function (error) {
+				    console.log(error);
+				  });
+			},
+			handleSelect(key, keyPath) {
+			        // console.log(key, keyPath);
+			      }
+		}
+	}
+</script>
---
+++ acmweb-vue/src/views/webHome.vue
@@ -0,0 +1,304 @@
+<template>
+  <div id="app">
+			<marquee class="player"  style="color: ivory;" >
+				公告:{{notice}}
+			</marquee>
+			<el-carousel :interval="5000" arrow="always" height="500px">
+			    <el-carousel-item v-for="item in showImageList" :key="item">
+			      <img :src="item.url" width="100%" height="100%">
+			    </el-carousel-item>
+			  </el-carousel>
+			  <div class="showintro">
+			  </div>
+			  <div class="line"></div>
+			  <div class="bottom">
+				  <!-- <el-row type="flex" class="row-bg" justify="space-around">
+				    <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+				    <el-col :span="6"><div class="grid-content bg-purple-light"></div></el-col>
+				    <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+				  </el-row> -->
+				  <div class="bottomitem">
+					  <div class="item"><div class="itemtitle">
+						<p>扫描右方二维码，关注我们</p>
+						<p>时刻获取最新资讯</p>
+					  </div>
+					  <div class="itemtitle">@2021技术部</div>
+				  		<!-- <img src="../assets/.png"> --></div>
+				  </div>
+				  <div class="bottomitem" style="border-right: 1px solid gray;border-left: 0.0625rem solid gray;">
+				  		<div class="itemtitle">沉思广场</div>
+				  		<img src="../assets/chensi.png">
+				  </div>
+				  <div class="bottomitem">
+				  		<div class="itemtitle"><a href="tencent://AddContact/?fromId=45&fromSubId=1&subcmd=all&uin=2725382040&website=www.oicqzone.com" style="color: white;">QQ公众号</a></div>
+						<img src="../assets/acm.png">
+				  </div>
+			  </div>
+  </div>
+</template>
+
+<style>
+	a {font-size:16px}
+	a:link {color: blue; text-decoration:none;} 
+	a:active:{color: red; } 
+	a:visited {color:purple;text-decoration:none;}  
+	a:hover {color: red; text-decoration:underline;} 
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+}
+.top{
+	width: 100%;
+	height: auto;
+}
+#nav {
+	width: 100%;
+}
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+.shownotice{
+	height: 17rem;
+	width: 40%;
+	background-color: #0969DA;
+}
+.el-carousel__item h3 {
+    color: #475669;
+    font-size: 18px;
+    opacity: 0.75;
+    line-height: 300px;
+    margin: 0;
+  }
+  
+  .el-carousel__item:nth-child(2n) {
+    background-color: #99a9bf;
+  }
+  
+  .el-carousel__item:nth-child(2n+1) {
+    background-color: #d3dce6;
+  }
+  img{
+    /*设置图片宽度和浏览器宽度一致*/
+    width: 100%;
+    height: inherit;
+  }
+.el-carousel{
+	overflow: hidden;
+		height: 60%;
+}
+
+.el-carousel__container{
+	width: 60%;
+	margin-left: 20%;
+	height: 50%;
+}
+.el-carousel__item{
+	width: 100%;
+}
+.player{
+	width: 100%;
+	height: 3.125rem;
+	filter: Alpha(Opacity=50); /* 针对IE*/
+	background-color: #003A79; 
+	top: 0%;
+	line-height: 3.125rem;
+}
+.el-carousel__item h1 {
+    color: #475669;
+    font-size: 18px;
+    opacity: 0.75;
+    line-height: 300px;
+    margin: 0;
+  }
+  .el-carousel__item:nth-child(2n) {
+    background-color: #D4DFE7;
+  }
+  .el-carousel__item:nth-child(2n+1) {
+    background-color: #FFFAE8;
+  }
+  .bannerImg{
+      width: 100%;
+      height: inherit;
+      min-height: 360px;
+      min-width: 1400px;
+    }
+	body{
+		width: 100%;
+		margin-left: 0;
+	}
+	.showintro{
+		box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+		width: 80%;
+		margin-left: 10%;
+		height: 500px;
+		background-color: #FFFFFF;
+		margin-top: 3%;
+	}
+	.notice{
+		width: 100%;
+		height: 3.125rem;
+		line-height: 3.125rem;
+		text-align: left;
+	}
+	@-webkit-keyframes horse {
+		0% {
+			left: 0%;
+			top: 0px;
+			background-color: lavenderblush;
+		}
+	 
+		47% {
+			left: 105%;
+			top: 0px;
+		}
+	 
+		48% {
+			left: 105%;
+			top: 100px;
+		}
+	 
+		49% {
+			left: -105%;
+			top: 100px;
+		}
+	 
+		50% {
+			left: -100%;
+			top: 0px;
+			background-color: lightpink;
+		}
+	 
+		100% {
+			left: 0%;
+			top: 0px;
+			background-color: #FFB6C1;
+		}
+	}
+	.line{
+		width: 100%;
+		height: 1px;
+		background-color: #9c9c9c;
+		margin-top: 3%;
+		margin-bottom: 3%;
+		margin-left: -3%;
+	}
+	.bottom{
+		width: 100%;
+		display: flex;
+		justify-content: space-around;
+		background-color: #003A79;
+		height: 35vh;
+		align-items: center;
+	}
+	.bottomitem{
+		width: 30%;
+		height: 30vh;
+	}
+	.bottomitem img{
+		margin-top: 10%;
+		width: 6.875rem;
+		height: 6.875rem;
+		height: auto;
+		background-color: white;
+	}
+	.itemtitle{
+		text-align: center;
+		font-weight: 300;
+		color:white;
+		width: 100%;
+		line-height: 3.125rem;
+		margin-top: 1%;
+		z-index: 99;
+	}
+	.qrcodeshow{
+		width: ;
+	}
+	
+</style>
+<script>
+	export default {
+	    data() {
+	      return {
+	        activeIndex: '1',
+	        activeIndex2: '1',
+			showImageList:[],
+			notice:'',
+			sum:'',
+			fileList:'',
+			imgHeight: '',
+			// 图片父容器高度
+			bannerHeight :1000,
+			// 浏览器宽度
+			screenWidth :0,
+			token:'eyJhbGciOiJIUzM4NCJ9.eyJzdWIiOiJhZG1pbiIsImlhdCI6MTY0MDYxMjM1MCwiZXhwIjoxNjQwNjk4NzUwfQ.I9L67Vg9iTew5gmmguTH-q6JJNt3RiCbpDPtyQc3JOec-kZ20cF4BshoO7HbPaDI'
+	      };
+	    },
+		
+		mounted() {
+		    
+		},
+		created() {
+			var _that=this
+			this.$axios.get('http://101.43.16.42:8082/notice').then((response)=>{
+					_that.notice=response.data.data.notice
+			        console.log(response);
+			      }).catch((response)=>{
+			        console.log(response);
+			      })
+			this.$axios.get('http://101.43.16.42:8082/rotation').then((response)=>{
+					_that.showImageList=response.data.data.urlList
+			        console.log(response);
+			      }).catch((response)=>{
+			        console.log(response);
+			      })
+			this.$axios.get('http://101.43.16.42:8082/introduce/file').then((response)=>{
+					_that.fileList=response.data.data.fileList
+			        console.log(response);
+			      }).catch((response)=>{
+			        console.log(response);
+			      })
+		},
+		 mounted() {
+		          // 首次加载时,需要调用一次
+		          this.screenWidth =  window.innerWidth;
+		          this.setSize();
+		          // 窗口大小发生改变时,调用一次
+		          window.onresize = () =>{
+		          this.screenWidth =  window.innerWidth;
+		          this.setSize();
+		        }
+			},
+		methods:{
+			getInfo(){
+				console.log('执行')
+				this.$axios.get('http://101.43.16.42:8082/notice')
+				  .then(function (response) {
+				    console.log(response);
+					console.log('已执行')
+				  })
+				  .catch(function (error) {
+				    console.log(error);
+				  });
+			},
+			canvasbg(){
+				
+			},
+			setSize:function () {
+			            // 通过浏览器宽度(图片宽度)计算高度
+			            this.bannerHeight = 400 / 1920 * this.screenWidth;
+			          },
+
+		}
+	}
+</script>
