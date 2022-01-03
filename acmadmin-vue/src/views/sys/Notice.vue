52193c21d3f4a74105da2faf10a8ccd62a7227eb
OREO-code <2319841923@qq.com>
2022-01-03 14:35:58 +0800

    提交代码

---
+++ acmadmin-vue/src/views/sys/Notice.vue
@@ -0,0 +1,58 @@
+<template>
+	<div>
+		<div>当前公告内容预览</div>
+		<marquee class="player"  style="color: ivory;" >
+			公告:{{notice}}
+		</marquee>
+		<el-input
+		  placeholder="请输入公告编辑内容"
+		  v-model="input"
+		  clearable>
+		</el-input>
+		<el-button type="primary">提交</el-button>
+	</div>
+</template>
+
+<script>
+	export default {
+	    data() {
+	      return {
+	        activeIndex: '1',
+	        activeIndex2: '1',
+			showImageList:[],
+			notice:'',
+			token:'',
+			input: ''
+	      };
+	    },
+		mounted() {
+		    
+		},
+		created() {
+			var _that=this
+			this.$axios.get('http://101.43.16.42:8082/notice').then((response)=>{
+					_that.notice=response.data.data.notice
+			        console.log(response);
+			      }).catch((response)=>{
+			        console.log(response);
+			      })
+		},
+		methods:{
+		},
+		}
+</script>
+
+<style>
+	.title{
+		width: 80%	;
+		margin-left: 10%;
+		}
+	.player{
+		width: 100%;
+		height: 3.125rem;
+		filter: Alpha(Opacity=50); /* 针对IE*/
+		background-color: #003A79; 
+		top: 0%;
+		line-height: 3.125rem;
+	}
+</style>
--- acmweb-vue/src/App.vue
+++ acmweb-vue/src/App.vue
@@ -8,18 +8,21 @@
 	  		          mode="horizontal"
 	  		          @select="handleSelect"
 	  		          >
-	  		        <el-menu-item index="1" route="/"><div >首页</div></el-menu-item>
+					  <el-menu-item><div> <img src="./assets/acmlogo.jpg" style="width: 3.875rem;height: 3.125rem;"></div></el-menu-item>
+	  		        <el-menu-item index="1" route="/">
+						<div ><i class="el-icon-house"></i>首页</div>
+					</el-menu-item>
 	  				<!-- <el-menu-item index="2" route="/time"><div >时间线</div></el-menu-item> -->
 	  				<!-- <el-menu-item index="3" route="/form">组织架构</el-menu-item> -->
-	  				<el-menu-item index="2" route="/member"><div>成员风采</div></el-menu-item>
+	  				<el-menu-item index="2" route="/member"><div><i class="el-icon-medal-1"></i>成员风采</div></el-menu-item>
 	  		        <!-- <el-submenu index="2">
 	  		          <template slot="title">组织架构</template>
 	  		          <el-menu-item index="2-1">竞赛队</el-menu-item>
 	  		          <el-menu-item index="2-2">技术部</el-menu-item>
 	  		          <el-menu-item index="2-3">预备队</el-menu-item>
 	  		        </el-submenu> -->
-	  		        <el-menu-item index="3" route="/tulin"><div>图灵杯</div></el-menu-item>
-	  		        <el-menu-item index="4"><a href="http://newoj.acmclub.cn/home" style="text-decoration: none;color: #A6ADB4;" target="_blank" ><div>NEUQ OJ</div></a>
+	  		        <el-menu-item index="3" route="/tulin"><div><i class="el-icon-trophy"></i>图灵杯</div></el-menu-item>
+	  		        <el-menu-item><a href="http://newoj.acmclub.cn/home" style="text-decoration: none;color: #A6ADB4;" target="_blank" ><div><i class="el-icon-link"></i>NEUQ OJ</div></a>
 	  		        </el-menu-item>
 					<!-- <el-menu-item index="7" route="/login"><div>管理员登录</div></el-menu-item> -->
 	  		        <!--        <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>-->
@@ -154,6 +157,11 @@
 	.showImage{
 		width: 100%;
 		height: 100%;
+	}
+	canvas {
+	  background-color: #eee;
+	  display: block;
+	  margin: 0 auto;
 	}
 </style>
 <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
