dce301acde87c28aacb1c5dbe71158e02c9b7ab7
zeroacmer <10791894@qq.com>
2022-01-04 17:57:53 +0800

    换个板娘！

--- acmweb-vue/src/App.vue
+++ acmweb-vue/src/App.vue
@@ -44,22 +44,22 @@
 			notice:'',
 	      };
 	    },
-    mounted() {
-      setTimeout(() => {
-        window.L2Dwidget.init({
-          pluginRootPath: 'live2dw/',
-          pluginJsPath: 'lib/',
-          pluginModelPath: 'live2d-widget-model-z16/assets/',
-          tagMode: false,
-          debug: false,
-          model: { jsonPath: '../live2dw/live2d-widget-model-shizuku/assets/shizuku.model.json' },
-          //public/live2dw/live2d-widget-model-shizuku/assets/shizuku.model.json
-          display: { position: 'left', width: 200, height: 400 },
-          mobile: { show: true },
-          log: false
-        })
-      }, 1000)
-    },
+    // mounted() {
+    //   setTimeout(() => {
+    //     window.L2Dwidget.init({
+    //       pluginRootPath: 'live2dw/',
+    //       pluginJsPath: 'lib/',
+    //       pluginModelPath: 'live2d-widget-model-z16/assets/',
+    //       tagMode: false,
+    //       debug: false,
+    //       model: { jsonPath: '../live2dw/live2d-widget-model-shizuku/assets/shizuku.model.json' },
+    //       //public/live2dw/live2d-widget-model-shizuku/assets/shizuku.model.json
+    //       display: { position: 'right', width: 200, height: 400 },
+    //       mobile: { show: true },
+    //       log: false
+    //     })
+    //   }, 1000)
+    // },
 
     methods:{
 			getInfo(){
