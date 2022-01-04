f3f8b7fad139a9b69d8b281cdc963e5650a84fa5
zeroacmer <10791894@qq.com>
2022-01-04 10:06:47 +0800

    前端代码融合

--- acmweb-vue/src/views/webHome.vue
+++ acmweb-vue/src/views/webHome.vue
@@ -1,18 +1,26 @@
 <template>
   <div id="app">
 
-    <el-container>
+    <el-container >
       <el-header>
         <marquee class="player"  style="color: ivory;" >
           公告:{{notice}}
         </marquee>
       </el-header>
       <el-main>
-        <el-carousel :interval="5000" arrow="always" height="35vw" >
-          <el-carousel-item v-for="item in showImageList" :key="item">
-            <img :src="item.url" width="100%" height="100%">
-          </el-carousel-item>
-        </el-carousel>
+        <el-container>
+          <el-aside width="200px">Aside</el-aside>
+          <el-main>
+
+            <el-carousel :interval="4000" type="card" height="200px">
+              <el-carousel-item v-for="item in showImageList" :key="item">
+                <el-image :src="item.url" style="position:absolute;top:0;bottom:0;left:0;right:0;width:100%;margin:auto;" fit="contain"></el-image>
+              </el-carousel-item>
+            </el-carousel>
+
+          </el-main>
+        </el-container>
+
         <div class="showintro">
           <router-view/>
         </div>
@@ -29,183 +37,10 @@
 </template>
 
 <style>
-	a {font-size:16px}
-	a:link {color: blue; text-decoration:none;}
-	a:active:{color: red; }
-	a:visited {color:purple;text-decoration:none;}
-	a:hover {color: red; text-decoration:underline;}
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  width: 100%;
-  margin: 0;
-  padding: 0;
-}
-.top{
-	width: 100%;
-	height: auto;
-}
-#nav {
-	width: 100%;
-}
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-.shownotice{
-	height: 17rem;
-	width: 40%;
-	background-color: #0969DA;
-}
-.el-carousel__item h3 {
-    color: #475669;
-    font-size: 18px;
-    opacity: 0.75;
-    line-height: 300px;
 
-  }
 
-  .el-carousel__item:nth-child(2n) {
-    background-color: #99a9bf;
-  }
 
-  .el-carousel__item:nth-child(2n+1) {
-    background-color: #d3dce6;
-  }
-  img{
-    /*设置图片宽度和浏览器宽度一致*/
-    width: 100%;
-    height: inherit;
-  }
-.el-carousel{
-	overflow: hidden;
-		height: 60%;
-}
-.player{
-	width: 100%;
-	height: 3.125rem;
-	filter: Alpha(Opacity=50); /* 针对IE*/
-	background-color: #003A79;
-	top: 0%;
-	line-height: 3.125rem;
-}
-.el-carousel__item h1 {
-    color: #475669;
-    font-size: 18px;
-    opacity: 0.75;
-    line-height: 300px;
-    margin: 0;
-  }
-  .el-carousel__item:nth-child(2n) {
-    background-color: #D4DFE7;
-  }
-  .el-carousel__item:nth-child(2n+1) {
-    background-color: #FFFAE8;
-  }
-  .bannerImg{
-      width: 100%;
-      height: inherit;
-      min-height: 360px;
-      min-width: 1400px;
-    }
-	body{
-		width: 100%;
-		margin-left: 0;
-	}
-	.showintro{
-		box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
-		width: 80%;
-		margin-left: 10%;
-		height: 500px;
-		background-color: #FFFFFF;
-		margin-top: 3%;
-	}
-	.notice{
-		width: 100%;
-		height: 3.125rem;
-		line-height: 3.125rem;
-		text-align: left;
-	}
-	@-webkit-keyframes horse {
-		0% {
-			left: 0%;
-			top: 0px;
-			background-color: lavenderblush;
-		}
-
-		47% {
-			left: 105%;
-			top: 0px;
-		}
-
-		48% {
-			left: 105%;
-			top: 100px;
-		}
-
-		49% {
-			left: -105%;
-			top: 100px;
-		}
-
-		50% {
-			left: -100%;
-			top: 0px;
-			background-color: lightpink;
-		}
-
-		100% {
-			left: 0%;
-			top: 0px;
-			background-color: #FFB6C1;
-		}
-	}
-	.line{
-		width: 100%;
-		height: 1px;
-		background-color: #9c9c9c;
-		margin-top: 3%;
-		margin-bottom: 3%;
-		margin-left: -3%;
-	}
-	.bottom{
-		width: 100%;
-		display: flex;
-		justify-content: space-around;
-		background-color: #003A79;
-		height: 35vh;
-		align-items: center;
-	}
-	.bottomitem{
-		width: 30%;
-		height: 30vh;
-	}
-	.bottomitem img{
-		margin-top: 10%;
-		width: 6.875rem;
-		height: 6.875rem;
-		height: auto;
-		background-color: white;
-	}
-	.itemtitle{
-		text-align: center;
-		font-weight: 300;
-		color:white;
-		width: 100%;
-		line-height: 3.125rem;
-		margin-top: 1%;
-		z-index: 99;
-	}
-	.qrcodeshow{
-		width: ;
-	}
 
 </style>
 <script>
