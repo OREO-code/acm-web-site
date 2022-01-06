462119f4c3aea2b95de1dcce6cc01fac7a52c47c
zeroacmer <10791894@qq.com>
2022-01-06 17:40:37 +0800

    update

--- acmweb-vue/src/views/member.vue
+++ acmweb-vue/src/views/member.vue
@@ -1,262 +1,289 @@
 <template>
-<div class="total">
-  <h3><span>成</span><span>员</span><span>风</span><span>采</span></h3>
-  <div class="groups">
+  <div class="total">
     <div class="group">
-      <div class="row row-first">
-        <div class="head">
+      <div class="body">
+        <div class="fxd">
           <h4><span class="title" title="年级">年级</span><span>></span></h4>
-          <h4><span class="title" title="学院">学院</span><span>></span></h4>
-        </div>
 
-          <div class="body">
-            <el-checkbox-group v-model="from.yearList" @change="getValue()">
+          <el-checkbox-group v-model="from.yearList" @change="getValue()">
             <div class="year row">
-            <el-checkbox  v-for="item in getYear"  :label="item"  :checked="false"  class="year-inner J_Ajax">
-            </el-checkbox>
-              </div>
-
-        </el-checkbox-group>
-            <el-checkbox-group v-model="from.collegeList" @change="getValue()">
-            <div class="college row collegec">
-              <el-checkbox  v-for="item in getCollege"  :label="item"  :checked="false"  class="college-inner J_Ajax">
+              <el-checkbox
+                  v-for="item in getYear"
+                  :label="item"
+                  :checked="false"
+                  class="year-inner J_Ajax"
+              >
               </el-checkbox>
-              </div>
-            </el-checkbox-group>
-          </div>
+            </div>
+          </el-checkbox-group>
+        </div>
+        <div class="fxd">
+          <h4><span class="title" title="学院">学院</span><span>></span></h4>
+
+          <el-checkbox-group v-model="from.collegeList" @change="getValue()">
+            <div class="college row collegec">
+              <el-checkbox
+                  v-for="item in getCollege"
+                  :label="item"
+                  :checked="false"
+                  class="college-inner J_Ajax"
+              >
+              </el-checkbox>
+            </div>
+          </el-checkbox-group>
+        </div>
       </div>
     </div>
-  </div>
 
-  <div class="people"  v-for="item in memberList" :key="item" width="100%">
-    <img class="picture"  src="item.url"></img>
-    <div class="introduction1" ><span>姓名：</span>{{item.name}}</div>
-    <div class="introduction2" ><span>年级：</span>{{item.year}}</div>
-    <div class="introduction3"><span>学院：</span>{{item.college}}</div>
-    <div class="introduction4"><span>荣誉：</span>{{item.honor}}</div>
-<!--    <img class="picture" src="../assets/images/1.png" width="100%"></img>-->
-<!--    <div class="introduction">东北大学悉尼智能科技学院</div>-->
+
+    <div class="people" v-for="item in memberList" :key="item" width="100%">
+      <el-container>
+        <el-aside width="200px">
+          <div class="ima">
+            <div class="block" v-for="fit in fits" :key="fit">
+              <el-image
+                  class="picture"
+                  :src="item.url"
+                  :fit="fit"></el-image>
+            </div>
+          </div>
+        </el-aside>
+        <el-main>
+          <div class="intro">
+            <div class="introduction1"><span>姓名：</span>{{ item.name }}</div>
+            <div class="introduction2"><span>年级：</span>{{ item.year }}</div>
+            <div class="introduction3"><span>学院：</span>{{ item.college }}</div>
+            <div class="introduction4"><span>简介：</span>{{ item.honor }}</div>
+          </div>
+        </el-main>
+      </el-container>
+    </div>
+
+
+    <!--    <div class="people" v-for="item in memberList" :key="item" width="100%">-->
+
+    <!--        <div class="ima">-->
+    <!--          <div class="block" v-for="fit in fits" :key="fit">-->
+    <!--            <el-image-->
+    <!--                class="picture"-->
+    <!--                :src="item.url"-->
+    <!--                :fit="fit"></el-image>-->
+    <!--          </div>-->
+    <!--        </div>-->
+
+    <!--      <div class="intro">-->
+    <!--      <div class="introduction1"><span>姓名：</span>{{ item.name }}</div>-->
+    <!--      <div class="introduction2"><span>年级：</span>{{ item.year }}</div>-->
+    <!--      <div class="introduction3"><span>学院：</span>{{ item.college }}</div>-->
+    <!--      <div class="introduction4"><span>简介：</span>{{ item.honor }}</div>-->
+    <!--      </div>-->
+    <!--      </div>-->
+
+
   </div>
-</div>
 </template>
 
 <script>
-var flag=0;
+var flag = 0;
 
-var img=document.querySelector('img');
-var div=document.getElementsByClassName('introduction')
+// var img=document.querySelector('img');
+// var div=document.getElementsByClassName('introduction')
 export default {
   name: "Show",
-  data(){
+  data() {
     return {
-      from:{
+      from: {
         yearList: [],
         collegeList: [],
       },
       memberList: [],
-      getYear:[],
-      getCollege:[]
-
-    }
-
+      getYear: [],
+      getCollege: [],
+      fits: ["contain"],
+    };
   },
 
-  mounted(){
-    document.querySelector('body').setAttribute('style',  'background:-webkit-linear-gradient(left,#6190E8,#A7BFE8)');
-  },
-  beforeDestroy() {
-    document.querySelector('body').removeAttribute('style')
-  },
+  // mounted(){
+  //   document.querySelector('body').setAttribute('style',  'background:-webkit-linear-gradient(left,#6190E8,#A7BFE8)');
+  // },
+  // beforeDestroy() {
+  //   document.querySelector('body').removeAttribute('style')
+  // },
   created() {
-    const _this=this;
-    _this.$axios.get("http://101.43.16.42:8082/getYear").then((response)=>{
+    const _this = this;
+    _this.$axios.get("http://101.43.16.42:8082/getYear").then((response) => {
       // console.log(response);
-      _this.getYear=response.data.data;
+      _this.getYear = response.data.data;
       //console.log(response.data.data)
-    })
-    _this.$axios.get("http://101.43.16.42:8082/getCollege").then((response)=>{
+    });
+    _this.$axios.get("http://101.43.16.42:8082/getCollege").then((response) => {
       // console.log(response);
-      _this.getCollege=response.data.data;
+      _this.getCollege = response.data.data
+      ;
 
       //console.log(response.data.data)
-    })
-
+    });
   },
-  methods:{
-    getValue(){
-      const _this= this;
-      // console.log(this.from)
-      // console.log(123456)
-      _this.$axios.post('http://101.43.16.42:8082/members',this.from).then((response)=>{
-        //console.log(response);
-        console.log(_this.from)
-        _this.memberList=response.data.data;
+  methods: {
+    getValue() {
+      const _this = this;
+      _this.$axios.post('http://101.43.16.42:8082/members', this.from).then((response) => {
+        _this.memberList = response.data.data;
+        // this.form.id=response.data.data[0].id;
         console.log(response)
-        console.log(_this.memberList)
-      })
+        // console.log(_this.memberList)
 
-    }
-  }
-}
+      })
+    },
+  },
+};
 </script>
 
 <style scoped>
-
-div{
-  display: block;
-}
-
-.group{
+.group {
   border: 1px solid #e8e8e8;
   border-radius: 7px;
-  margin-top: -38px;
-  box-shadow:0 10px 50px 0px #ADA996 ;
+  margin-top: 98px;
+  margin-left: 45px;
+  padding-left: 23px;
+  box-shadow: 0 2px 5px 0px #ada996;
+  position: absolute;
+  left: 0;
+  display: flex;
+  align-items: flex-start;
+  flex-wrap: wrap;
+  justify-content: flex-start;
 }
 
-.group .row{
-  position: relative;
-  margin: 0 8px;
+.group .head {
+  color: black;
 }
-.group .row-first, .group .row:first-child {
-  border-top: none;
-}
-.group .head{
-  position: absolute;
-  left: 11px;
-  top: 9px;
-  color: black
-}
-.collegec{
-  position: absolute !important;
-  top:54px;
-  left:124px;
-  width:500px;
-}
-.group .year{
-  position:absolute;
-  left:123px;
-  bottom: 189px;
-}
-.head h4{
-  font-weight: 400;
-  font-size: 100%;
-}
-h4{
-  font-size: 100%;
-}
-.group .head .title{
-  float: left;
+.group .head .title {
   max-width: 80px;
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
 }
 
-.group .body{
-  height: 36px;
-  width:500px;
-  overflow: hidden;
-  padding:0 116px 210px ;
+.year {
+  display: flex;
+  flex-direction: column;
+}
+.collegec {
+  width: 281px;
+}
+.fxd {
+  display: flex;
 }
 
-.group .body .btn{
+.head h4 {
+  font-weight: 400;
+  font-size: 100%;
+}
+h4 {
+  font-size: 100%;
+}
+
+.group .body .btn {
   display: none;
   text-align: center;
 }
 .group .body .year .college {
-  float: left;
   margin: 9px 40px 9px 0;
   height: 18px;
+  line-height: 18px;
   color: black;
   text-decoration: none;
 }
-.college{
+.college {
   float: left;
-  margin: 9px 40px 9px 0;
-  height: 18px;
+  margin: -5px 41px 0px 48px;
   color: black;
   text-decoration: none;
 }
 
- .year-inner{
-   text-decoration: none;
-   color: #403131;
-   float: left;
-   margin: 28px 85px 9px 0;
-   height: 18px;
-   text-decoration: none;
-
- }
- .college-inner{
-   text-decoration: none;
-   color: #403131;
-   float: left;
-   margin: 21px 40px 9px 0;
-   height: 18px;
-   text-decoration: none;
- }
-
- .people{
-   position: relative;
-   margin: 10px;
-   border:  1px solid rgba(173, 171, 171, 0.5);
-   border-radius: 7px;
-   width: 978px;
-   height: 166px;
-   left: -9px;
-   /*background: -webkit-linear-gradient(left,#6190E8,#021B79);*/
-   background: rgba(164, 167, 164, 0.4);
-   color: rgb(0,0,0);
- }
- img{
-   height: 148px;
-   width: 118px;
-   float: left;
-   padding-top: 9px;
-   padding-left: 12px;
- }
- .introduction1,.introduction2,.introduction3,.introduction4{
-   width: 800px;
-   height:3px;
-   position: relative;
-   padding: 18px;
-   left: 121px;
- }
- h3{
-   font-size: 3em;
-   font-weight: 500;
-   color: #222;
-   letter-spacing: 5px;
-   text-align: center;
-   cursor:pointer;
-   font-family: 'Poppons';
-   margin-top: 2px;
- }
- h3 span{
-   transition: 0.5s;
- }
- h3:hover span:nth-child(1){
-   margin-right: 10px;
- }
-h3:hover span:nth-child(3){
-  margin-left: 40px;
+.year-inner {
+  text-decoration: none;
+  color: #403131;
+  margin: 22px 85px 9px 46px;
+  height: 18px;
+  text-decoration: none;
 }
-h3:hover span{
-  color: #fff;
-  text-shadow: 0 0 10px #733ebf;
+.college-inner {
+  text-decoration: none;
+  color: #403131;
+  margin: 21px 40px 9px 0;
+  height: 18px;
+  text-decoration: none;
 }
-.group{
 
-  color: #FFFFFF;
+.people {
+  position: relative;
+  margin: 12px;
+  border: 1px solid rgba(173, 171, 171, 0.5);
+  border-radius: 7px;
+  width: 937px;
+  /*height: 166px;*/
+  left: 245px;
+  top: 97px;
+  /*background: -webkit-linear-gradient(left,#6190E8,#021B79);*/
+  /*background: rgba(164, 167, 164, 0.4);*/
+  color: rgb(0, 0, 0);
+  box-shadow: 0 2px 5px 0px #ada996;
 }
-h4 span{
-  color: #FFFFFF;
+.ima {
+  /*height: 148px;*/
+  width: 150px;
+  float: left;
+  padding-top: 36px;
+  padding-left: 15px;
+  /*display: flex;*/
+}
+
+/*.people{*/
+/*  display: flex;*/
+/*  flex-direction: column;*/
+/*  !*flex-wrap:wrap;*!*/
+/*  !*align-items: flex-start;*!*/
+/*  !*justify-content: flex-start;*!*/
+/*}*/
+.intro{
+  display: flex;
+  flex-direction: column;
+  flex-wrap:wrap;
+  align-items: flex-start;
+  justify-content: flex-start;
+  margin-left: -213px;
+  overflow-x: hidden;
+}
+
+.introduction1,
+.introduction2,
+.introduction3,
+.introduction4 {
+  width: 800px;
+  height: 3px;
+  position: relative;
+  padding: 11px;
+  left: 121px;
+  padding-left: 133px;
+  flex: 1;
+}
+.introduction4{
+  width: 560px;
+}
+/*.intro{*/
+/*  flex: 1;*/
+/*}*/
+
+h4 span {
+  color: black;
 }
 .people {
   transition: 0.5s;
 }
-.people:hover{
-  box-shadow:  0 0 20px #FFFFFF;
+.people:hover {
+  box-shadow: 0 0 20px #ffffff;
   margin-left: 40px;
 }
 </style>
