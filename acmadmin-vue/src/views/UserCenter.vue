a2f8cbc403d58f92589ed3b4fcd9159d7a43eff9
zeroacmer <10791894@qq.com>
2022-01-07 02:27:46 +0800

    蚌埠住了

--- acmadmin-vue/src/views/UserCenter.vue
+++ acmadmin-vue/src/views/UserCenter.vue
@@ -1,38 +1,78 @@
 <template>
-	<div style="text-align: center;">
-		<h2>你好！{{ userInfo.username }} 同学</h2>
+  <el-container>
+    <el-header style="text-align: center">
+      <h2>你好！{{ userInfo.username }} 同学</h2>
+    </el-header>
+
+    <el-card class="box-card" style="text-align: center">
+
+      <el-row>
+        <el-col :span="12"><div class="grid-content bg-purple">
+          <el-card class="box-card" style="width: 90%;">
+            <h2>个人信息</h2>
+            <el-descriptions class="margin-top" :column="1" style="text-align: center" >
+              <el-descriptions-item label="ID" style="text-align: center !important;">{{userInfo.id}}</el-descriptions-item>
+              <el-descriptions-item label="用户名">{{userInfo.username}}</el-descriptions-item>
+              <el-descriptions-item label="邮箱">{{userInfo.email}}</el-descriptions-item>
+              <el-descriptions-item label="手机号">{{userInfo.phone}}</el-descriptions-item>
+              <el-descriptions-item label="备注">{{userInfo.remarks}}</el-descriptions-item>
+              <el-descriptions-item label="操作"> <el-button
+                  type="success"
+                  @click="open">编辑</el-button></el-descriptions-item>
+
+            </el-descriptions>
+          </el-card>
+        </div></el-col>
+        <el-col :span="12"><div class="grid-content bg-purple-light">
+          <el-card class="box-card" style="width: 90%">
+            <h2>修改密码</h2>
+            <el-form :model="passForm" status-icon :rules="passRules" ref="passForm" label-width="100px">
+              <el-form-item label="旧密码" prop="currentPass">
+                <el-input type="password" v-model="passForm.oldPassword" autocomplete="off"></el-input>
+              </el-form-item>
+              <el-form-item label="新密码" prop="password">
+                <el-input type="password" v-model="passForm.newPassword1" autocomplete="off"></el-input>
+              </el-form-item>
+              <el-form-item label="确认密码" prop="checkPass">
+                <el-input type="password" v-model="passForm.newPassword2" autocomplete="off"></el-input>
+              </el-form-item>
+              <el-form-item>
+                <el-button type="primary" @click="submitPasswordForm('passForm')">提交</el-button>
+                <el-button @click="resetForm('passForm')">重置</el-button>
+              </el-form-item>
+            </el-form>
+          </el-card>
+        </div></el-col>
+      </el-row>
 
 
-        <el-descriptions class="margin-top" title="基本信息" :column="1" >
+    </el-card>
 
-          <el-descriptions-item label="用户名">{{userInfo.username}}</el-descriptions-item>
-          <el-descriptions-item label="ID">{{userInfo.id}}</el-descriptions-item>
-          <el-descriptions-item label="邮箱">{{userInfo.email}}</el-descriptions-item>
-          <el-descriptions-item label="手机号">{{userInfo.phone}}</el-descriptions-item>
-          <el-descriptions-item label="备注">{{userInfo.remarks}}</el-descriptions-item>
-        </el-descriptions>
+    <el-dialog
+        title="修改管理员信息"
+        :visible.sync="dialogVisible"
+        width="30%"
+        :before-close="handleClose">
+      <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+        <el-form-item label="用户名" prop="content">
+          <el-input v-model="ruleForm.username"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" prop="email">
+          <el-input v-model="ruleForm.email"></el-input>
+        </el-form-item>
+        <el-form-item label="手机号" prop="phone">
+          <el-input v-model="ruleForm.phone"></el-input>
+        </el-form-item>
+        <el-form-item label="备注" prop="remarks">
+          <el-input v-model="ruleForm.remarks"></el-input>
+        </el-form-item>
+        <el-form-item style="text-align: center">
+          <el-button type="primary" @click="submitForm('ruleForm')">立即修改</el-button>
+        </el-form-item>
+      </el-form>
 
-        <el-form :model="passForm" status-icon :rules="rules" ref="passForm" label-width="100px">
-          <el-form-item label="旧密码" prop="currentPass">
-            <el-input type="password" v-model="passForm.currentPass" autocomplete="off"></el-input>
-          </el-form-item>
-          <el-form-item label="新密码" prop="password">
-            <el-input type="password" v-model="passForm.password" autocomplete="off"></el-input>
-          </el-form-item>
-          <el-form-item label="确认密码" prop="checkPass">
-            <el-input type="password" v-model="passForm.checkPass" autocomplete="off"></el-input>
-          </el-form-item>
-          <el-form-item>
-            <el-button type="primary" @click="submitForm('passForm')">提交</el-button>
-            <el-button @click="resetForm('passForm')">重置</el-button>
-          </el-form-item>
-        </el-form>
-
-
-
-		  
-
-	</div>
+    </el-dialog>
+  </el-container>
 </template>
 
 <script>
@@ -50,23 +90,47 @@
 				}
 			};
 			return {
+        dialogVisible: false,
+        ruleForm: {
+          id:"",
+          username:"",
+          password:"",
+          email:"",
+          phone:"",
+          remarks:""
+        },
+        rules: {
+          username: [
+            { required: true, message: '请输入姓名', trigger: 'blur' }
+          ],
+          email: [
+            { required: true, message: '请输入邮箱', trigger: 'blur' }
+          ],
+          phone: [
+            { required: true, message: '请输入手机号', trigger: 'blur' }
+          ],
+          remarks: [
+            { required: true, message: '请输入备注', trigger: 'blur' }
+          ],
+        },
 				userInfo: {
 
 				},
 				passForm: {
-					password: '',
-					checkPass: '',
-					currentPass: ''
+          username:'',
+          newPassword1: '',
+          newPassword2: '',
+          oldPassword: ''
 				},
-				rules: {
-					password: [
+				passRules: {
+          newPassword1: [
 						{ required: true, message: '请输入新密码', trigger: 'blur' },
 						{ min: 6, max: 12, message: '长度在 6 到 12 个字符', trigger: 'blur' }
 					],
-					checkPass: [
+          newPassword2: [
 						{ required: true, validator: validatePass, trigger: 'blur' }
 					],
-					currentPass: [
+          oldPassword: [
 						{ required: true, message: '请输入当前密码', trigger: 'blur' },
 					]
 				}
@@ -80,18 +144,47 @@
 			getUserInfo() {
 				this.$axios.get("/currentUser",localStorage.token).then(res =>{
 					this.userInfo=res.data.data
+          this.ruleForm.id = res.data.data.id;
+          this.ruleForm.username = res.data.data.username;
+          this.passForm.username = res.data.data.username;
+          this.ruleForm.email = res.data.data.email;
+          this.ruleForm.phone = res.data.data.phone;
+          this.ruleForm.remarks = res.data.data.remarks;
 				})
-				// this.$axios.get("/sys/userInfo").then(res => {
 
-				// 	this.userInfo = res.data.data;
-				// })
 			},
-			submitForm(formName) {
+      open(){
+        this.dialogVisible = true
+
+      },
+      submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            this.dialogVisible = false
+            const _this = this;
+            this.$axios.post("/updateUser",_this.ruleForm).then(res=>{
+              this.getUserInfo()
+              this.$message({
+                message: '修改成功',
+                type: 'success'
+              });
+            }).catch(reason => {
+              this.$message.error(reason);
+            })
+
+          } else {
+            this.$message.error('error submit!!');
+            return false;
+          }
+        });
+      },
+			submitPasswordForm(formName) {
 				this.$refs[formName].validate((valid) => {
 					if (valid) {
 
 						const _this = this
-						this.$axios.post('/sys/user/updatePass', this.passForm).then(res => {
+            console.log(_this.passForm)
+						this.$axios.post('/updatePassword', _this.passForm).then(res => {
 
 							_this.$alert(res.data.msg, '提示', {
 								confirmButtonText: '确定',
--- acmadmin-vue/src/views/sys/Rotation.vue
+++ acmadmin-vue/src/views/sys/Rotation.vue
@@ -1,25 +1,15 @@
 <template>
   <div>
     <template>
+      <div class="headbar">
+        轮播图修改界面
+      </div>
       <el-carousel :interval="4000" type="card" height="200px">
-        <el-carousel-item v-for="item in showImageList" :key="item">
+        <el-carousel-item v-for="(item,index) in showImageList" :key="index">
           <el-image :src="item.url" style="position:absolute;top:0;bottom:0;left:0;right:0;width:100%;margin:auto;" fit="contain"></el-image>
         </el-carousel-item>
       </el-carousel>
     </template>
-    <!-- <div class="operate">
-      <div>
-        <el-upload
-          class="upload-demo"
-          drag
-          action="https://jsonplaceholder.typicode.com/posts/"
-          multiple>
-          <i class="el-icon-upload"></i>
-          <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
-          <div class="el-upload__tip" slot="tip">请注意图片尺寸</div>
-        </el-upload>
-      </div>
-    </div> -->
     <div class="imgshow">
       <div v-for="item in showImageList" class="imgbtn">
         <div >
@@ -30,6 +20,31 @@
           <el-button type="danger" icon="el-icon-delete" circle @click="getvalue(item.id)" ></el-button>
         </div>
       </div>
+      <div class="imgbtn">
+        <el-upload
+            ref="upload"
+            action="http://101.43.16.42:8082/addRotation"
+            :before-upload="beforeAvatarUpload"
+            :headers="myHeaders"
+
+            :limit="1"
+            :on-preview="handlePictureCardPreview"
+            :on-remove="handleRemove"
+            :on-success="GetUrl">
+          <i class="el-icon-plus"></i>
+        </el-upload>
+      </div>
+      <!-- <el-upload
+         class="avatar-uploader"
+         action="http://101.43.16.42:8082/addRotation"
+       :on-error="uploaderr"
+       :headers="headers"
+         :show-file-list="false"
+         :on-success="handleAvatarSuccess"
+         :before-upload="beforeAvatarUpload">
+         <img v-if="imageUrl" :src="imageUrl" class="avatar">
+         <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+       </el-upload> -->
     </div>
   </div>
 </template>
@@ -38,7 +53,7 @@
 export default {
   data() {
     return {
-
+      imageUrl: '',
       showImageList:[],
       sum:'',
       imgHeight: '',
@@ -47,7 +62,16 @@ export default {
       // 浏览器宽度
       screenWidth :0,
       token:'',
-      fileList: []
+      fileList: [],
+      deleteid:{
+        id:''
+      },
+      uploaddata:{
+
+      },
+      myHeaders:{
+        'Authorization' : localStorage.getItem("token")
+      },
     };
   },
 
@@ -74,14 +98,23 @@ export default {
   },
   methods:{
     getvalue(value){
+      var _that=this
       console.log(value)
+      this.deleteid.id=value
       this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
         confirmButtonText: '确定',
         cancelButtonText: '取消',
         type: 'warning'
       }).then(() => {
-        this.$axios.get('http://101.43.16.42:8082/rotation',{params:{id:value}}).then((response)=>{
-          _that.showImageList=response.data.data.urlList
+        console.log(JSON.stringify(this.deleteid))
+        this.$axios.post('/delRotation',JSON.stringify(this.deleteid)).then((response)=>{
+          this.$axios.get('http://101.43.16.42:8082/rotation').then((response)=>{
+            _that.showImageList=response.data.data.urlList
+            console.log(response);
+          }).catch((response)=>{
+            console.log('err')
+            console.log(response);
+          })
           console.log(response);
         }).catch((response)=>{
           console.log('err')
@@ -99,7 +132,6 @@ export default {
       this.$axios.get('http://101.43.16.42:8082/notice')
           .then(function (response) {
             console.log(response);
-            console.log('已执行')
           })
           .catch(function (error) {
             console.log(error);
@@ -127,6 +159,7 @@ export default {
     },
 
     delete(value){
+      var _that=this
       this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
         confirmButtonText: '确定',
         cancelButtonText: '取消',
@@ -147,24 +180,84 @@ export default {
       });
 
     },
+    handleAvatarSuccess(res, file, fileList) {
+      var _that=this
+      console.log('上传成功')
+      console.log(res);
+      console.log(file)
+      this.$message({
+        showClose: true,
+        message: res
+      });
+      this.$axios.get('http://101.43.16.42:8082/rotation').then((response)=>{
+        _that.showImageList=response.data.data.urlList
+        console.log(response);
+      }).catch((response)=>{
+        console.log('err')
+        console.log(response);
+      })
+    },
+    beforeAvatarUpload(file) {
+      const isJPG = file.type === 'image/jpeg';
+      const isPNG = file.type === 'image/png';
+      const isSVG = file.type === 'image/svg';
+      const isLt100M = file.size / 1024 / 1024 < 100;
+      let extension = file.name.substring(file.name.lastIndexOf('.')+1)
+      if ((extension!='jpg')&&(extension!='png')&&(extension!='svg')) {
+        this.$message.error('上传图片只能是jpg或png或svg格式!');
+      }
+      if (!isLt100M) {
+        this.$message.error('上传头像大小不能超过 100MB!');
+      }
+      return (isJPG||isPNG||isSVG) && isLt100M;
+    },
+    uploaderr(err, file, fileList){
+      console.log(err)
+      this.$message.error('上传失败:'+err);
+    },
+    //1
+    GetUrl(response, file, fileList){
+      // this.ruleForm.url = response.data
+      var _that=this
+      this.$refs.upload.clearFiles();
+      this.$axios.get('http://101.43.16.42:8082/rotation').then((response)=>{
+        _that.showImageList=response.data.data.urlList
+        console.log(response);
+      }).catch((response)=>{
+        console.log('err')
+        console.log(response);
+      })
+
+    },
     handleRemove(file, fileList) {
       console.log(file, fileList);
+
     },
-    handlePreview(file) {
-      console.log(file);
+    handlePictureCardPreview(file) {
+      console.log(file.url)
+      //this.dialogImageUrl = file.url;
+      this.dialogVisibleImage = true;
     },
-    // handleExceed(files, fileList) {
-    //   this.$message.warning(`当前限制选择 3 个文件，本次选择了 ${files.length} 个文件，共选择了 ${files.length + fileList.length} 个文件`);
-    // },
-    beforeRemove(file, fileList) {
-      return this.$confirm(`确定移除 ${ file.name }？`);
-    }
+
 
 
   }
 }
 </script>
 <style>
+.headbar{
+  width: 100%;
+  /* 		box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1); */
+  margin-top: 1%;
+  height: 10%;
+  padding-top: 2%;
+  padding-bottom: 2%;
+  margin-bottom: 3%;
+  font-size: 1.5625rem;
+  text-align: center;
+  font-weight: bold;
+  border-bottom: 0.0625rem solid #409EFF;
+}
 .operate{
   width: 80%;
   margin-left: 10%;
@@ -185,7 +278,7 @@ export default {
   margin-top: 1%;
   display: grid;
   grid-template-columns: repeat(3, 1fr);
-  grid-auto-rows: 250px;
+  grid-auto-rows: 300px;
   grid-gap: 25px;
   box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
   margin-bottom: 5%;
@@ -204,5 +297,44 @@ export default {
 }
 .deletebtn{
   margin-left: 0.625rem;
+}
+//上传部分
+  .avatar-uploader .el-upload {
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+
+  }
+
+.el-upload.el-upload--text{
+  width: 100% !important;
+  height: 100% !important;
+
+}
+.el-icon-plus.avatar-uploader-icon{
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1)
+}
+.avatar-uploader .el-upload:hover {
+  border-color: #409EFF;
+}
+.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  line-height: 178px;
+  text-align: center;
+}
+.avatar {
+  width: 178px;
+  height: 178px;
+  display: block;
 }
 </style>
